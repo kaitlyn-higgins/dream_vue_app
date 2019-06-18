3b64623861b8bbdce2981b81ad446f538ef91cac
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-06-18 14:05:42 -0500

    Creates Dreams view folder, dreams new file and configs router for dreams new

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import UsersNew from './views/UsersNew.vue';
+import DreamsNew from './views/Dreams/New.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -16,10 +19,25 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: '/users/new',
+      name: 'users-new',
+      component: UsersNew
+    },
+    {
+      path: '/dreams/new',
+      name: 'dreams-new',
+      component: DreamsNew
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
-})
+});
