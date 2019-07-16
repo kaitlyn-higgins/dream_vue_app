f0afdc34d562959a87fdfd5b406ac4d3f022b78a
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-07-16 18:35:33 -0500

    Configures side bar in users show, sticky

--- src/views/Users/Show.vue
+++ src/views/Users/Show.vue
@@ -99,6 +99,25 @@
               
                 <router-link v-bind:to="'/users/' + user.id + '/edit'" class="btn btn-raised btn-info animated fadeInUp animation-delay-10"><i class="zmdi zmdi-email"></i> Edit Account Info</router-link>
               </div>
+
+
+              <form class="mw-500 center-block animated fadeInUp">
+                <input type="text" placeholder="Search for dreams" v-model="dreamFilter" class="form-control color-white">
+                <button type="button" class="btn btn-raised btn-primary btn-block"><i class="zmdi zmdi-search"></i> Search</button>
+              </form>
+
+              <div>
+                <button v-on:click="setSortAttribute('updated_at')" class="btn btn-raised btn-royal">Sort by Date</button>
+                  <i v-if="sortAttribute === 'updated_at' && sortAscending === 1">^</i>
+                  <i v-if="sortAttribute === 'updated_at' && sortAscending === -1"></i>
+
+              </div>
+
+              <!-- <div>
+                <button v-for="theme in themes" v-on:click="setSortAttribute('theme.name')">{{theme.name}}</button>
+              </div> -->
+
+
             </div>
           </div>
         </div>
@@ -108,7 +127,7 @@
 
         <div class="container">
           <div class="row">
-            <div v-for="dream in orderBy(filterBy(user.dreams, dreamFilter, 'title', 'content', 'tags', 'themes'), sortAttribute, sortAscending)" class="col-lg-8">
+            <div  class="col-lg-8">
 
 
 
@@ -118,7 +137,7 @@
 
 
 <!-- cards -->
-              <article class="card wow fadeInLeft animation-delay-5 mb-4">
+              <article v-for="dream in orderBy(filterBy(user.dreams, dreamFilter, 'title', 'content', 'tags', 'themes'), sortAttribute, sortAscending)" class="card wow fadeInLeft animation-delay-5 mb-4">
                 <div class="card-body overflow-hidden">
                   <div class="row">
                     <div class="col-lg-6">
@@ -263,10 +282,11 @@
 
 
     <!-- column of stuff-->
-            <div class="col-lg-4">
+
+            <div class="col-lg-4 column-container">
 
       <!-- photo -->   
-              <div class="card animated fadeInUp animation-delay-7">
+              <div class="card animated fadeInUp animation-delay-7 user-box">
                 <div class="ms-hero-bg-info ms-hero-img-mountain">
                   <h3 class="color-white index-1 text-center no-m pt-4">{{user.username}}</h3>
 
@@ -285,20 +305,20 @@
 
 
     <!-- navigation -->
-              <div class="card card-primary animated fadeInUp animation-delay-7">
+              <div class="card card-primary animated fadeInUp animation-delay-7 navigation-box">
                 <div class="card-header">
                   <h3 class="card-title"><i class="zmdi zmdi-apps"></i> Navigation</h3>
                 </div>
                 <div class="card-tabs">
                   <ul class="nav nav-tabs nav-tabs-transparent indicator-primary nav-tabs-full nav-tabs-4" role="tablist">
-                    <li class="nav-item"><a href="#favorite" aria-controls="favorite" role="tab" data-toggle="tab" class="nav-link withoutripple active"><i class="no-mr zmdi zmdi-star"></i></a></li>
+                    <!-- <li class="nav-item"><a href="#favorite" aria-controls="favorite" role="tab" data-toggle="tab" class="nav-link withoutripple active"><i class="no-mr zmdi zmdi-star"></i></a></li> -->
                     <li class="nav-item"><a href="#categories" aria-controls="categories" role="tab" data-toggle="tab" class="nav-link withoutripple"><i class="no-mr zmdi zmdi-folder"></i></a></li>
                     <li class="nav-item"><a href="#archives" aria-controls="archives" role="tab" data-toggle="tab" class="nav-link withoutripple"><i class="no-mr zmdi zmdi-time"></i></a></li>
                     <li class="nav-item"><a href="#tags" aria-controls="tags" role="tab" data-toggle="tab" class="nav-link withoutripple"><i class="no-mr zmdi zmdi-tag-more"></i></a></li>
                   </ul>
                 </div>
                 <div class="tab-content">
-                  <div role="tabpanel" class="tab-pane fade active show" id="favorite">
+                  <!-- <div role="tabpanel" class="tab-pane fade active show" id="favorite">
                     <div class="card-body overflow-hidden">
                       <div class="ms-media-list">
                         <div class="media mb-2">
@@ -345,14 +365,48 @@
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </div> -->
                   <div role="tabpanel" class="tab-pane fade" id="categories">
-                    <div class="list-group">
-                      <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-info zmdi zmdi-cocktail"></i>Design <span class="ml-auto badge-pill badge-pill-info">25</span></a>
-                      <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-warning zmdi zmdi-collection-image"></i> Graphics <span class="ml-auto badge-pill badge-pill-warning">14</span></a>
+
+
+                    <!-- filter search bar -->
+
+                    <!-- <div>
+                      <label for="dream-filter" v-if="isCurrentUser()">Search your dreams</label>
+                      <label for="dream-filter" v-else>Search {{user.username}}'s dreams</label>
+                      <input type="text" placeholder="Enter search term" v-model="dreamFilter" list="titles">
+                    </div>
+
+
+
+                    <label for="dream-filter">Find dreams by theme</label>
+                    <span v-for="theme in themes">
+                      <input type="radio" id="theme.name" :value="theme.name" v-model="dreamFilter"> {{ theme.name }}
+                    </span> -->
+
+
+                    <!-- sorting buttons -->
+
+                    <!-- <div>
+                      <button v-on:click="setSortAttribute('updated_at')">Sort by Date</button>
+                        <i v-if="sortAttribute === 'updated_at' && sortAscending === 1">^</i>
+                        <i v-if="sortAttribute === 'updated_at' && sortAscending === -1"></i>
+                      <button v-on:click="setSortAttribute('lucid')">Lucid</button>
+                    </div> -->
+
+                    <div v-for="theme in themes" class="list-group">
+
+
+                      <!-- <input id="theme.name" :value="theme.name"  class="list-group-item list-group-item-action withripple"><i class=" color-info zmdi zmdi-cocktail"></i>{{theme.name}}<span class="ml-auto badge-pill badge-pill-info">25</span> -->
+
+                      <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-info zmdi zmdi-cocktail"></i>{{theme.name}}<span class="ml-auto badge-pill badge-pill-info">25</span></a>
+
+
+
+                      <!-- <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-warning zmdi zmdi-collection-image"></i> Graphics <span class="ml-auto badge-pill badge-pill-warning">14</span></a>
                       <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-danger zmdi zmdi-case-check"></i> Productivity <span class="ml-auto badge-pill badge-pill-danger">7</span></a>
                       <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-royal zmdi zmdi-folder-star-alt"></i>Resources <span class="ml-auto badge-pill badge-pill-royal">67</span></a>
-                      <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-success zmdi zmdi-play-circle-outline"></i>Multimedia <span class="ml-auto badge-pill badge-pill-success">32</span></a>
+                      <a href="javascript:void(0)" class="list-group-item list-group-item-action withripple"><i class=" color-success zmdi zmdi-play-circle-outline"></i>Multimedia <span class="ml-auto badge-pill badge-pill-success">32</span></a> -->
                     </div>
                   </div>
                   <div role="tabpanel" class="tab-pane fade" id="archives">
@@ -422,6 +476,24 @@
   </div>
 </template>
 
+<style>
+  
+.column-container {
+  align-items: flex-start;
+}
+
+.user-box {
+  position: sticky;
+  top: 8rem;
+}
+
+.navigation-box {
+  position: sticky;
+  top: 34rem;
+}
+
+
+</style>
 
 <script>
 import axios from "axios";
